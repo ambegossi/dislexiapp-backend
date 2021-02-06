a5567aa4510a0cf71cd8d63b20d3c036499ba106
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-06 16:10:05 -0300

    :label:(chore) update id column type

--- src/database/migrations/1612635394151-CreateUsers.ts
+++ src/database/migrations/1612635394151-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1612635394151 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
