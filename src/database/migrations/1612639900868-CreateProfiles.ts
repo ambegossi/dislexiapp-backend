a3ac33458a489d2327f49290aa6b796b6223d69f
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-06 17:05:00 -0300

    :card_file_box:(feat) generate create profiles migration

---
+++ src/database/migrations/1612639900868-CreateProfiles.ts
@@ -0,0 +1,67 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateProfiles1612639900868 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'profiles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'score',
+            type: 'integer',
+            default: 0,
+          },
+          {
+            name: 'level',
+            type: 'integer',
+            default: 0,
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'profiles',
+      new TableForeignKey({
+        name: 'UserProfile',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('profiles', 'UserProfile');
+
+    await queryRunner.dropTable('profiles');
+  }
+}
