6ba1406ec936aa6159460e924bf8eca3b8924153
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-07 14:40:17 -0300

    :sparkles:(feat) create sessions route

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { name, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      name,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      profile_id: user.profile_id,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ userWithoutPassword });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
