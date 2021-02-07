4b99d497409fb8bfc788004311649567ad002215
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-07 14:52:03 -0300

    :sparkles:(feat) return jwt token on new session

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,7 +10,7 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUser = new AuthenticateUserService();
 
-    const { user } = await authenticateUser.execute({
+    const { user, token } = await authenticateUser.execute({
       name,
       password,
     });
@@ -23,7 +23,7 @@ sessionsRouter.post('/', async (request, response) => {
       updated_at: user.updated_at,
     };
 
-    return response.json({ userWithoutPassword });
+    return response.json({ userWithoutPassword, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect name/password combination.');
     }
 
-    return { user };
+    const token = sign({}, '0c2ad0f74124148ecccc67a0916b0f2c', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
