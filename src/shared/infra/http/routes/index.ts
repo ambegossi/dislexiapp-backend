f616bdbbd50998598e5e1f18ef60395040884ba0
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-27 18:14:41 -0300

    :sparkles:(feat) create profile routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/profile', profileRouter);
 routes.use('/sessions', sessionsRouter);
 
 export default routes;
