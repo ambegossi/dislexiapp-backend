b87be7c05bf95c0247db881faacb349459bbb0fa
Anderson Begossi <andersonbegossi@gmail.com>
2021-02-27 18:13:52 -0300

    :sparkles:(feat) create ProfileController

---
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const showProfile = container.resolve(ShowProfileService);
+
+    const { user, profile } = await showProfile.execute({ user_id });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      profile_id: user.profile_id,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+      profile,
+    };
+
+    return response.json(userWithoutPassword);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const { name, old_password, password } = request.body;
+
+    const updateProfile = container.resolve(UpdateProfileService);
+
+    const nameLowerCase = name.toLowerCase();
+
+    const user = await updateProfile.execute({
+      user_id,
+      name: nameLowerCase,
+      old_password,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      profile_id: user.profile_id,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  }
+}
