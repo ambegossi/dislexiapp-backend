a9b323edbfd057cdce7de6e6b9cde73ab05f94eb
Anderson Begossi <andersonbegossi@gmail.com>
2021-03-27 18:15:20 -0300

    :building_construction:(chore) create StorageProvider index.ts

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,1 @@
-import { container } from 'tsyringe';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
+import './StorageProvider';
